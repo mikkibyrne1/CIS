0507678b372193245c7ba6bae33bf407fcb78d4c
mikkibyrne1 <48295756+mikkibyrne1@users.noreply.github.com>
2019-03-22 13:43:48 -0500

    Update Input.java

--- Input.java
+++ Input.java
@@ -14,35 +14,40 @@ public class Input
         Scanner cc =  new Scanner(System.in);
         Scanner sg =  new Scanner(System.in);
         Scanner ccred = new Scanner(System.in);
+        Scanner y = new Scanner(System.in);
         Student[] stud = new Student[3];
         for (int i=0; i < stud.length; i++) {
             System.out.println ("-----Enter a StudentID-----");
             stud[i]= new Student();
             stud[i].setStudentNum(cc.nextInt());
             System.out.println(stud[i].getStudentNum());
-            for (int d=0; d < 5; d++){
+            for (int d=0; d < 2; d++){
                 System.out.println ("-----Enter a Course-----");
                 stud[i].setCourseID(sg.nextLine(),d);
                 System.out.println(stud[i].getCourse(d).getCourseID());
-                
+
                 System.out.println ("-----Enter a Credit-----");
                 stud[i].setCredits(ccred.nextInt(),d);
                 System.out.println(stud[i].getCourseCredits(d).getCredits());
-                
+
                 System.out.println ("-----Enter a Grade for class-----");
-                stud[i].setCredits(ccred.nextInt(),d);
+                stud[i].setGrades(y.next().charAt(0),d);
                 System.out.println(stud[i].getGrade(d).getGrade());
-                
 
+                //System.out.println("Your first period class is " + stud[i].getCourse(d).getCourseID() + ". This class is worth " + stud[i].getCourseCredits(d).getCredits() + " college credits. Your grade for this class is a/an " + stud[i].getGrade(d).getGrade());
             }
-            /*for (int m=0; m < 5; m++){
-            System.out.println ("---Enter a Credit---");
-            stud[i].setCredits(sg.nextInt(),m);
-            System.out.println(stud[i].getCourse(m).getCredits());
-             */
 
-            
-            
+        }
+        for (int i=0; i<stud.length; i++){
+            System.out.println("Student #" + stud[i].getStudentNum());
+            System.out.println("Course ID # \tCredit hour \t Grade");
+            for (int c = 0; c<2; c++){
+                System.out.println((c+1)+ "--" + "\t" +
+                    stud[i].getCourse(c).getCourseID() + "\t"+
+                    stud[i].getCourseCredits(c).getCredits() + "\t" +
+                    stud[i].getGrade(c).getGrade());
+                  
+                }
         }
     }
 }
